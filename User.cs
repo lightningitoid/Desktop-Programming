0253eb8283b88b3236ea1e505f1817ca984898a5
itoid <master20ranger@gmail.com>
2024-04-29 10:43:56 +0700

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,16 @@
+﻿public class User
+{
+    private string Username { get; set; }
+    private string Password { get; set; }
+
+    public User(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+
+    public bool Authenticate(string username, string password)
+    {
+        return Username == username && Password == password;
+    }
+}
